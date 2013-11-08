7bc65405c09a1ae07517bb0fe249443f57a6becf
Martin Wimpress <code@flexion.org>
2013-11-08 10:31:21 +0000

    Added .md to Geany.

--- packages/app/geany.sh
+++ packages/app/geany.sh
@@ -6,10 +6,13 @@ if [ `id -u` -ne 0 ]; then
 fi
 
 CORE_PKG=$(basename ${0} .sh)
-MORE_PKGS="$(basename ${0} .sh)-plugins"
+MORE_PKGS="$(basename ${0} .sh)-plugins markdown"
 
 pacman -S --needed --noconfirm ${CORE_PKG} ${MORE_PKGS}
 
 mkdir -p /home/${SUDO_USER}/.config/geany/filedefs
 wget -qO- http://download.geany.org/contrib/oblivion2.tar.gz | tar zxv -C /home/${SUDO_USER}/.config/geany/filedefs/
+cp /home/${SUDO_USER}/.config/geany/filedefs/filetypes.markdown /home/${SUDO_USER}/.config/geany/filedefs/filetypes.md
+sed -i 's/extension=markdown/extension=md/' /home/${SUDO_USER}/.config/geany/filedefs/filetypes.md
 chown -R ${SUDO_USER}: /home/${SUDO_USER}/.config/geany/filedefs
+chmod 644 /home/${SUDO_USER}/.config/geany/filedefs/*
