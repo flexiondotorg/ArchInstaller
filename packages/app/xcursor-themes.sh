c3184951be871dd6a10cf757e754bfec58a527be
Martin Wimpress <code@flexion.org>
2013-12-09 14:15:52 +0000

    Package updates. Compliance with official package updates.

---
+++ packages/app/xcursor-themes.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ `id -u` -ne 0 ]; then
+    echo "ERROR! `basename ${0}` must be executed as root."
+    exit 1
+fi
+
+CORE_PKG=$(basename ${0} .sh)
+MORE_PKGS="xcursor-bluecurve xcursor-flatbed xcursor-neutral xcursor-pinux xcursor-premium xcursor-simpleandsoft xcursor-vanilla-dmz xcursor-vanilla-dmz-aa"
+
+pacman -S --needed --noconfirm ${CORE_PKG} ${MORE_PKGS}
